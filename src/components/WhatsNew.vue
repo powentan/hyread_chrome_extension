415cb507a15114c9f6e9c787b44838cdab71e759
Powen Tan <powentan0330@gmail.com>
2023-09-11 12:43:03 +0800

    feat: add what's new component

---
+++ src/components/WhatsNew.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-dialog
+      :model-value="isShow"
+      :title="title"
+      width="30%"
+    >
+        <slot name="body"/>
+        <template #footer>
+        <span class="dialog-footer">
+            <el-button @click="handleClose">關閉</el-button>
+            <el-button type="primary" @click="handleClose">
+            知道了
+            </el-button>
+        </span>
+        </template>
+    </el-dialog>
+</template>
+
+<script setup lang="ts">
+defineProps(['title', 'isShow']);
+const emit = defineEmits(['close-dialog']);
+
+function handleClose() {
+    emit('close-dialog');
+}
+</script>
