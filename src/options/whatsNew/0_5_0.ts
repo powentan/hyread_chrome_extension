924e7a5ee95275fb425b2c1dbad941ae7c696c97
譚博文Powen Tan <powen.tan@goodfinance.com>
2023-09-13 23:23:20 +0800

    chore: bump into v0.5.0 version and update what's new info

---
+++ src/options/whatsNew/0_5_0.ts
@@ -0,0 +1,43 @@
+const contentBody: string = `
+## v0.5.0 新功能：
+1. 更新設定畫面版面
+2. 設定儲存自動重讀已經開啟的hyread網頁
+3. 新增"閱讀進度"百分比資訊
+4. 新增3種匯出格式
+    - HyRead格式 (---分隔)
+
+        \`\`\`markdown
+        ## XXX
+        XXXXXXX
+        ---
+        \`\`\`
+    - HQ&A
+        \`\`\`markdown
+        ## Q1
+        A: 
+
+        > Highlight
+        \`\`\`
+    - 顏色標題
+
+        \`\`\`markdown
+        ## color1
+        XXXX
+        ---
+        XXX
+        ## color2
+        XXXX
+        ---
+        XXX
+        ## color3
+        XXX
+        ---
+        \`\`\`
+    
+## 回饋
+如果覺得我做得好請幫忙給予回饋：<a href="https://chrome.google.com/webstore/detail/hyread%E5%9C%96%E6%9B%B8%E9%A4%A8%E8%A8%BB%E8%A8%98%E5%8C%AF%E5%87%BA/jhneiikmjdmkjpcoggnbnofednphlamm?hl=zh-TW" target="_blank">chrome線上應用程式</a>
+
+有建議也可以留言在：<a href="https://www.youtube.com/@powentan/videos" target="_blank">youtube頻道</a>
+`;
+
+export default contentBody;
