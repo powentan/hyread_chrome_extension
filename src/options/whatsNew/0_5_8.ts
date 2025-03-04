57ba6c779b684769d4f40016f20e0dafc692fb3c
Powen Tan <powentan0330@gmail.com>
2025-03-04 12:55:23 +0800

    fix: adjust markdown format for notion importing (#13)
    Co-authored-by: Powen Tan <powentan@Powens-MacBook-Pro.local>

---
+++ src/options/whatsNew/0_5_8.ts
@@ -0,0 +1,22 @@
+const contentBody: string = `
+
+## v0.5.8:
+### 問題修正
+- 部分格式(類HyRead格式、定義顏色標題、定義劃線風格)，markdown檔案**匯入notion**會出現格式錯誤，感謝網友回報
+
+## v0.5.7:
+### 問題修正
+- 無法正常從**線上閱讀**匯出，感謝網友回報
+
+## 回饋
+如果覺得我做得好請幫忙給予回饋：<a href="https://chrome.google.com/webstore/detail/hyread%E5%9C%96%E6%9B%B8%E9%A4%A8%E8%A8%BB%E8%A8%98%E5%8C%AF%E5%87%BA/jhneiikmjdmkjpcoggnbnofednphlamm?hl=zh-TW" target="_blank">chrome線上應用程式</a>
+
+## 問題需求回報
+可以直接透過email找我，或是任何線上可以接觸到我的方式。
+
+
+## Donate
+如果覺得我做得好想給我一點鼓勵：<a href="https://www.buymeacoffee.com/powentan" target="_blank">Buy Me a Coffee</a>
+`;
+
+export default contentBody;
